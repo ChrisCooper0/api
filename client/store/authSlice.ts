0a93411bb3f43351434247b7757f3453b676c199
ChrisCooper0 <christopher.cooper@mail.com>
2022-11-08 09:45:41 +0000

    add extra reducer

--- client/store/authSlice.ts
+++ client/store/authSlice.ts
@@ -22,6 +22,16 @@ export const authSlice = createSlice({
       state.authState = action.payload;
     },
   },
+
+  // Special reducer for hydrating the state. Special case for next-redux-wrapper
+  extraReducers: {
+    [HYDRATE]: (state, action) => {
+      return {
+        ...state,
+        ...action.payload.auth,
+      };
+    },
+  },
 });
 
 export const { setAuthState } = authSlice.actions;
