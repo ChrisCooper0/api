5a44c6bbcd0884e82e71bd08d4f194938c00fec9
ChrisCooper0 <christopher.cooper@mail.com>
2023-01-05 21:12:34 +0000

    docker update

--- server/dist/dbConnection.js
+++ server/dist/dbConnection.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 var mysql2_1 = require("mysql2");
 exports.db = (0, mysql2_1.createConnection)({
-    host: "localhost",
+    host: "127.0.0.1",
     user: "root",
     password: "password",
     database: "api",
+    port: 3306,
 });
