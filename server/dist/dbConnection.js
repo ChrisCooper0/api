fb539631aeb6b3f4e3e89ea1a29e9f65845c43fe
ChrisCooper0 <christopher.cooper@mail.com>
2023-01-07 11:27:31 +0000

    docker rm

--- server/dist/dbConnection.js
+++ server/dist/dbConnection.js
@@ -1,10 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 var mysql2_1 = require("mysql2");
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 exports.db = (0, mysql2_1.createConnection)({
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     user: "root",
-    password: "password",
-    database: "api",
+    password: "P05t1tn0t3",
+    database: process.env.DB_NAME,
 });
