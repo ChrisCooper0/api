074a8d92eb3bebb43e10129fe0f5676d58848a69
ChrisCooper0 <christopher.cooper@mail.com>
2023-02-12 20:47:51 +0000

    add express-rate-limit

--- server/dist/server.js
+++ server/dist/server.js
@@ -6,13 +6,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var helmet_1 = __importDefault(require("helmet"));
 var cors_1 = __importDefault(require("cors"));
+var express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 var routes_1 = __importDefault(require("./routes/routes"));
-var PORT = 8080;
+var limiter = (0, express_rate_limit_1.default)({
+    windowMs: 15 * 60 * 1000,
+    max: 2,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: "Stop sending requests please!",
+});
 var app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
+app.use(limiter);
 app.use("/api", routes_1.default);
-app.listen(PORT, function () {
-    console.log("\uD83D\uDE80 Server started on port ".concat(PORT));
+app.listen(process.env.PORT, function () {
+    console.log("\uD83D\uDE80 Server started on port ".concat(process.env.PORT));
 });
--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,26 @@
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 import apiRoutes from "./routes/routes";
 
-const PORT = 8080;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(limiter);
 
 app.use("/api", apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server started on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`🚀 Server started on port ${process.env.PORT}`);
 });
